e23f418238db74a415f96cdaf98bb7614c7411a0
mygoare <mygoare@gmail.com>
2014-10-09 18:11:25 +0800

    add e

---
+++ e.py
@@ -0,0 +1,9 @@
+from math import e
+ 
+def e_with_precision(n):
+    return '%.*f' % (n, e)
+if __name__ == '__main__':
+    while True:
+        precision = int(raw_input('Number of decimal places: '))
+        print(e_with_precision(precision))
+            
