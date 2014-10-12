8db01632a4144ce98d46a2c5420e239aba60aecc
ranlei <ranleixxx@gmail.com>
2014-10-12 13:39:28 +0800

    add one caculator

---
+++ cacl.py
@@ -0,0 +1,26 @@
+def cacl(a,b,op):
+    if op not in '+-*/':
+        return "Please only type one of these characters '+-*/'"
+
+    if op == '+':
+        return str(str(a)+op+str(b)+'='+str(a+b))
+    if op == '-':
+        return str(str(a)+op+str(b)+'='+str(a-b))
+    if op == '*':
+        return str(str(a)+op+str(b)+'='+str(a*b))
+    if op == '/':
+        return str(str(a)+op+str(b)+'='+str(a//b))
+
+if __name__ ==  "__main__":
+    flag = True
+    while flag:
+        pass
+        try:
+            a = input("please this first number:")
+        except:
+            print "maybe you should input number." 
+        b = input("please this second number:")
+        op = raw_input("give me you want excution(+-*/):")
+        if a and b and op:
+            flag = False
+            print cacl(a,b,op)
